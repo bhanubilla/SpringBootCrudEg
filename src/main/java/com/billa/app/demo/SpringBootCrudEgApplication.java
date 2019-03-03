4e291396ad994a8193b2cb7a7e2bd1185e3ea8b6
billa <billa@192.168.2.5>
2019-03-03 20:17:58 +0530

    first commit

---
+++ src/main/java/com/billa/app/demo/SpringBootCrudEgApplication.java
@@ -0,0 +1,14 @@
+package com.billa.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudEgApplication.class, args);
+	}
+
+}
+
