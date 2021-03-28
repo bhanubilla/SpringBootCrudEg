be233c8f0ba8edbfa38ad31f296240b4564c43b8
Bhanu Billa <bhanuprasad.b@digitelenetworks.com>
2021-03-28 19:07:00 +0530

    third commit

--- src/main/java/com/billa/app/demo/SpringBootCrudEgApplication.java
+++ src/main/java/com/billa/app/demo/SpringBootCrudEgApplication.java
@@ -8,7 +8,6 @@ public class SpringBootCrudEgApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrudEgApplication.class, args);
-		System.out.println("second commit");
 	}
 
 }
